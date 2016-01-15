01f5d354fa556c7886c7a723af970856194c84dc
Ceri Storey <cez@necrofish.org.uk>
2016-01-15 20:22:02 +0000

    Ensure that we only write the version iff it has changed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 extern crate git2;
 #[macro_use]
 extern crate quick_error;
+
 use git2::{Repository, DescribeOptions};
 use std::env;
 use std::convert::AsRef;
 use std::fs::File;
-use std::io::{self,Write, BufWriter};
+use std::io::{Write, Read, BufWriter};
 use std::path::Path;
 
 quick_error! {
@@ -21,15 +22,39 @@ quick_error! {
         }
     }
 }
+
+
+fn same_content_as<P: AsRef<Path>>(path: P, content: &str) -> Result<bool, Error> {
+
+    let mut f = try!(File::open(path));
+    let mut current = String::new();
+    try!(f.read_to_string(&mut current));
+
+    Ok(current == content)
+}
+
 pub fn write_version <P: AsRef<Path>>(topdir: P) -> Result<(), Error> {
     let path = try!(env::var_os("OUT_DIR").ok_or(Error::MissingEnvVar));
     let path : &Path = path.as_ref();
 
     let path = path.join("version.rs");
-    let mut file = BufWriter::new(try!(File::create(&path)));
 
     let repo = try!(Repository::open(topdir));
     let desc = try!(repo.describe(&DescribeOptions::new()));
-    try!(writeln!(file, r#"static VERSION: &'static str = {:?};"#, desc.format(None).unwrap()));
+
+
+    let content = format!("static VERSION: &'static str = {:?};\n", try!(desc.format(None)));
+
+    let is_fresh = if path.exists() {
+        try!(same_content_as(&path, &content))
+    } else {
+        false
+    };
+
+    if !is_fresh {
+      let mut file = BufWriter::new(try!(File::create(&path)));
+
+      try!(write!(file, "{}", content));
+    }
     Ok(())
 }
