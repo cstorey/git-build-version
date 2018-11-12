725ce472771a87be80d5a5651f85d0c8631aeec8
Ceri Storey <cez@necrofish.org.uk>
2018-11-12 21:52:38 +0000

    Kinda get the tests to work usefully.

--- src/lib.rs
+++ src/lib.rs
@@ -36,25 +36,28 @@ fn same_content_as<P: AsRef<Path>>(path: P, content: &str) -> Result<bool, Error
 pub fn write_version <P: AsRef<Path>>(topdir: P) -> Result<(), Error> {
     let path = try!(env::var_os("OUT_DIR").ok_or(Error::MissingEnvVar));
     let path : &Path = path.as_ref();
+    write_version_into(topdir.as_ref(), path.as_ref())
+}
 
+
+pub fn write_version_into(topdir: &Path, path: &Path) -> Result<(), Error> {
     try!(create_dir_all(path));
 
-    let path = path.join("version.rs");
-
+    let verfile = path.join("version.rs");
     let repo = try!(Repository::discover(topdir));
     let desc = try!(repo.describe(&DescribeOptions::new().describe_tags().show_commit_oid_as_fallback(true)));
 
 
     let content = format!("static VERSION: &'static str = {:?};\n", try!(desc.format(None)));
 
-    let is_fresh = if path.exists() {
-        try!(same_content_as(&path, &content))
+    let is_fresh = if verfile.exists() {
+        try!(same_content_as(&verfile, &content))
     } else {
         false
     };
 
     if !is_fresh {
-      let mut file = BufWriter::new(try!(File::create(&path)));
+      let mut file = BufWriter::new(try!(File::create(&verfile)));
 
       try!(write!(file, "{}", content));
     }
@@ -63,5 +66,5 @@ pub fn write_version <P: AsRef<Path>>(topdir: P) -> Result<(), Error> {
 
 #[test]
 fn test() {
-    write_version(".").expect("write version");
+    write_version_into(Path::new("."), Path::new("/tmp/git-build-version")).expect("write version");
 }
